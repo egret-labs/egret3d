a18cead17e50dfe77cf1efd2721fe587bd39bfd6
duanchunlei <duanchunlei@egret.com>
2018-07-04 17:13:40 +0800

    update light

---
+++ egret3d/components/light/BaseLight.ts
@@ -0,0 +1,187 @@
+namespace egret3d {
+    /**
+     * Light Type Enum
+     * @version paper 1.0
+     * @platform Web
+     * @language en_US
+     */
+    /**
+     * 灯光类型的枚举。
+     * @version paper 1.0
+     * @platform Web
+     * @language
+     */
+    export const enum LightType {
+        /**
+         * direction light
+         * @version paper 1.0
+         * @platform Web
+         * @language en_US
+         */
+        /**
+         * 直射光
+         * @version paper 1.0
+         * @platform Web
+         * @language
+         */
+        Direction = 1,
+        /**
+         * point light
+         * @version paper 1.0
+         * @platform Web
+         * @language en_US
+         */
+        /**
+         * 点光源
+         * @version paper 1.0
+         * @platform Web
+         * @language
+         */
+        Point = 2,
+        /**
+         * point light
+         * @version paper 1.0
+         * @platform Web
+         * @language en_US
+         */
+        /**
+         * 聚光灯
+         * @version paper 1.0
+         * @platform Web
+         * @language
+         */
+        Spot = 3,
+    }
+    /**
+     * light component
+     * @version paper 1.0
+     * @platform Web
+     * @language en_US
+     */
+    /**
+     * 灯光组件
+     * @version paper 1.0
+     * @platform Web
+     * @language
+     */
+    export abstract class BaseLight extends paper.BaseComponent {
+        /**
+         * 
+         */
+        public readonly type: LightType;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.CHECKBOX)
+        public castShadows: boolean = false;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public intensity: number = 2;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public distance: number = 50;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public decay: number = 2;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public angle: number = Math.PI / 6;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public penumbra: number = 0;
+        /**
+         * spot angel cos
+         * @version paper 1.0
+         * @platform Web
+         * @language en_US
+         */
+        /**
+         * 聚光灯的开合角度cos值
+         * @version paper 1.0
+         * @platform Web
+         * @language
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public spotAngelCos: number = 0.9;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public shadowBias: number = 0.0003;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public shadowRadius: number = 2;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public shadowSize: number = 16;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public shadowCameraNear: number = 0.1;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.NUMBER)
+        public shadowCameraFar: number = 200;
+        /**
+         * 
+         */
+        @paper.serializedField
+        @paper.editor.property(paper.editor.EditType.COLOR)
+        public readonly color: Color = new Color(1.0, 1.0, 1.0, 1.0);
+        public readonly matrix: Matrix = new Matrix();
+        public renderTarget: IRenderTarget;
+
+        protected _updateMatrix(camera: Camera) {
+            // matrix * 0.5 + 0.5, after identity, range is 0 ~ 1 instead of -1 ~ 1
+            const matrix = this.matrix;
+            matrix.set(
+                0.5, 0.0, 0.0, 0.5,
+                0.0, 0.5, 0.0, 0.5,
+                0.0, 0.0, 0.5, 0.5,
+                0.0, 0.0, 0.0, 1.0
+            );
+
+            camera.calcProjectMatrix(512 / 512, helpMatrixA);
+            helpMatrixB.copy(this.gameObject.transform.getWorldMatrix()).inverse();
+            Matrix.multiply(matrix, helpMatrixA, matrix);
+            Matrix.multiply(matrix, helpMatrixB, matrix);
+        }
+
+        public update(camera: Camera, faceIndex: number) {
+            camera.opvalue = 1.0;
+            camera.backgroundColor.set(1.0, 1.0, 1.0, 1.0);
+            camera.clearOption_Color = true;
+            camera.clearOption_Depth = true;
+
+            this._updateMatrix(camera);
+        }
+    }
+}
---
+++ egret3d/components/light/LightSystem.ts
@@ -0,0 +1,71 @@
+namespace egret3d {
+    /**
+     * Light系统
+     */
+    export class LightSystem extends paper.BaseSystem<BaseLight> {
+        protected readonly _interests = [
+            { componentClass: [DirectLight, SpotLight, PointLight] }
+        ];
+        private readonly _lightCamera: Camera = this._globalGameObject.getComponent(Camera) || this._globalGameObject.addComponent(Camera);
+        private readonly _drawCalls: DrawCalls = this._globalGameObject.getComponent(DrawCalls) || this._globalGameObject.addComponent(DrawCalls);
+
+        public onUpdate() {
+            const camera = this._lightCamera;
+            const drawCalls = this._drawCalls.drawCalls;
+
+            for (const light of this._components) {
+                if (!light.castShadows) {
+                    continue;;
+                }
+
+                let faceCount = 1;
+
+                if (light.type === LightType.Point) {
+                    faceCount = 6;
+                }
+                else if (light.type === LightType.Direction) {
+                }
+                else if (light.type === LightType.Spot) {
+                }
+
+                for (let i = 0; i < faceCount; ++i) {
+                    (light.renderTarget as GlRenderTargetCube).activeCubeFace = i; // TODO 创建接口。
+                    light.update(camera, i);
+                    camera._targetAndViewport(light.renderTarget, false);
+
+                    // render shadow
+                    const context = camera.context;
+                    if (light.type === LightType.Point) {
+                        context.drawtype = "_distance_package";
+                    }
+                    else {
+                        context.drawtype = "_depth_package";
+                    }
+
+                    context.updateCamera(camera, light.matrix);
+                    context.updateLightDepth(light);
+
+                    for (const drawCall of drawCalls) {
+                        if (!drawCall.renderer.castShadows) {
+                            continue;
+                        }
+
+                        const gameObject = drawCall.renderer.gameObject;
+                        context.drawCall = drawCall;
+                        context.updateModel(drawCall.matrix || gameObject.transform.getWorldMatrix());
+                        //
+                        let drawType = "base";
+                        if (drawCall.boneData) {
+                            context.updateBones(drawCall.boneData);
+                            drawType = "skin";
+                        }
+
+                        WebGLKit.draw(context, drawType);
+                    }
+                }
+
+                GlRenderTarget.useNull(WebGLKit.webgl);
+            }
+        }
+    }
+}
