75bf874341476c6d8dd71470386599ea58de7e31
dcl <akdcl@sina.com>
2018-09-26 11:53:02 +0800

    update README.md

--- examples/src/examples/Test.ts
+++ examples/src/examples/Test.ts
@@ -2,9 +2,5 @@ namespace Test {
     export async function start() {
         // Create camera.
         egret3d.Camera.main;
-
-        const abc = egret3d.DefaultMeshes.createObject(egret3d.DefaultMeshes.CUBE_LINE);
-
-        console.log("Test");
     }
 }
