6b34f867118ef833a91ee45bae08f2958f84e253
许亮 <xuliang@egret.com>
2019-01-22 15:49:30 +0800

    update

--- examples/resource/shaders/thermaVision/thermaVision_frag.glsl
+++ examples/resource/shaders/thermaVision/thermaVision_frag.glsl
@@ -12,10 +12,7 @@ void main(){
     colors[2] = vec4(1.,0.,0.,1.);
     float lum = (pixcol.r+pixcol.g+pixcol.b)/3.;
     vec4 thermal;
-    // if (lum<thermaValue) thermal = mix(colors[0],colors[2],(lum-float(0)*thermaValue)/thermaValue);
-    // if (lum>=thermaValue) thermal = mix(colors[1],colors[2],(lum-float(1)*thermaValue)/thermaValue);
-    // gl_FragColor = thermal;
-    int threshold = (lum < thermaValue)? 0:1; 
-    vec3 result = lerp(colors[threshold], colors[threshold + 1], (lum-float(threshold) * 0.5f) / 0.5f);
-    gl_FragColor = vec4(result, 1.0);
+    if (lum<thermaValue) thermal = mix(colors[0],colors[2],(lum-float(0)*thermaValue)/thermaValue);
+    if (lum>=thermaValue) thermal = mix(colors[1],colors[2],(lum-float(1)*thermaValue)/thermaValue);
+    gl_FragColor = thermal;
 }
