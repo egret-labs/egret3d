1cf4c0adac29fe96a4591b00a07dfe883713165d
许亮 <xuliang@egret.com>
2019-01-28 16:51:38 +0800

    update particle_frag.glsl

--- core/egret3d/asset/default/shaders/particle_frag.glsl
+++ core/egret3d/asset/default/shaders/particle_frag.glsl
@@ -8,13 +8,13 @@ varying float v_discard;
 varying vec4 v_color;
 varying vec2 v_texcoord;
 
-#ifdef RENDERMODE_MESH
+#ifdef RENDERMESH
 	varying vec4 v_mesh_color;
 #endif
 
 void main()
 {	
-	#ifdef RENDERMODE_MESH
+	#ifdef RENDERMESH
 		gl_FragColor=v_mesh_color;
 	#else
 		gl_FragColor=vec4(1.0);	
